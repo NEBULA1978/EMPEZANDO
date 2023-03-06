72dbc8b910e6b969faacd5f2ed4bb24d5db21213
NEBULA1978 <rpascualey@gmail.com>
2023-03-07 00:51:07 +0100

    Creando archivo base app.vue dos variables nombre y edad y saludamos y le decimos la edad ejemplo con dos inputs

---
+++ src/components/App2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>HOLA {{ miNombre }} y tendré {{ miEdad }}</h1>
+    <label>Edad 1:</label>
+    <input type="number" v-model="miEdad" />
+    <!-- <label>Edad 2:</label>
+    <input type="number" v-model="miEdad" /> -->
+  </div>
+</template>
+
+<script setup>
+const miNombre = "Ramon"
+let miEdad = 28
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
